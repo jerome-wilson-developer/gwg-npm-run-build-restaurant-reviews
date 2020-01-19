7996561d9c9ddb562bb0e227dfd9209f8ef2d86e
jerome-wilson-developer <jerome-wilson-developer@hotmail.com>
2020-01-19 11:47:35 -0500

    edits and update.

--- mws-restaurant-stage-1-master-JAN3THU/js/mapboxKey.js
+++
@@ -1,6 +0,0 @@
-class mapboxKey {
-    static get mapbox_key(){
-        //return your mapbox API key
-        return "pk.eyJ1IjoiZHJqd2Vic2l0ZWRlc2lnbiIsImEiOiJjam4ycHlxY3QwcGIyM2tuZHNwaDR3Y2c3In0.HYhXd_N7m2m7gLkCzWovEw";
-    }
-}
